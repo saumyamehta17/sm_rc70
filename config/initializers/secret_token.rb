44f1af2383c7e160f2742d9059f325964f91404a
sweety <smehta1289@gmail.com>
2013-06-02 20:32:16 +0530

    searching records by year passed in url

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmRc70::Application.config.secret_token = '6d30e3ff35dd168ed681c6a0424b89fc47c324bd7afd08e2c95c712e62b0c3b9fb692b608484cafa6912929ee1d7af4dd2d1fa1d05ed6ae3af4546e9ab94a4f8'
